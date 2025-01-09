f926f88d19f0b5a20d074dae044a2c816897fd16
Georgii Lapshin <georgiilapshin@Georgiis-MacBook-Air.local>
2025-01-09 13:16:59 +0100

    ebz

--- GNL2.0/get_next_line.h
+++ GNL2.0/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: glapshin <glapshin@student.42prague.com    +#+  +:+       +#+        */
+/*   By: georgiilapshin <georgiilapshin@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 19:15:06 by glapshin          #+#    #+#             */
-/*   Updated: 2024/12/09 20:08:53 by glapshin         ###   ########.fr       */
+/*   Updated: 2025/01/05 14:44:38 by georgiilaps      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,7 @@
 void	*ft_memset(void *b, int c, size_t len);
 char	*get_next_line(int fd);
 void	*ft_calloc(size_t count, size_t size);
-
+char	*ft_strchr( const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
 # endif
--- GNL2.0/get_next_line_utils.c
+++ GNL2.0/get_next_line_utils.c
@@ -33,3 +33,37 @@ void	*ft_memset(void *b, int c, size_t len)
 		*temp++ = (unsigned char)c;
 	return (b);
 }
+char	*ft_strchr( const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return ((char *)s);
+	}
+	return (NULL);
+}
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	char	*tmp;
+
+	if (!s1 || !s2)
+		return (NULL);
+	ptr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!ptr)
+		return (NULL);
+	tmp = ptr;
+	while (*s1)
+		*tmp++ = *s1++;
+	while (*s2)
+		*tmp++ = *s2++;
+	*tmp = '\0';
+	return (ptr);
+}
